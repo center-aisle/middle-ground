6d5ee0f8582298f0ce3923c1ff295bb91cc303db
NAYWOOD <naywood@Naomis-MacBook-Pro.local>
2019-05-10 17:50:43 -0700

    component folders and base file made

---
+++ src/components/body/body.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function body() {
+  return (
+    <div className="main-container">
+      
+    </div>
+  );
+}
+
+export default body;
---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function footer() {
+  return (
+    <div className="main-container">
+      
+    </div>
+  );
+}
+
+export default footer;
