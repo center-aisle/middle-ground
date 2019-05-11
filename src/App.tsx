ce40f44a0d7afd7d5ae10bd0ae915b3ed22478d5
NAYWOOD <naywood@Naomis-MacBook-Pro.local>
2019-05-10 18:36:37 -0700

    the footer functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import Footer from './components/footer/footer';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      
+      <Footer/>
     </div>
   );
 }
