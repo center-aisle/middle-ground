ea6f28bfb9f99b492a78b2edd9746863e59da307
Efreets <44280987+bryanrkelley@users.noreply.github.com>
2019-05-15 19:54:06 -0700

    saved the header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,13 +2,8 @@ import React from 'react';
 
 function Header() {
   return (
-<<<<<<< HEAD
-    <div className='main-container'>
-
-=======
     <div className="main-container">
       <p>Here</p>
->>>>>>> 7f22651637d17a23dd2a226684d9b52af42679ef
     </div>
   );
 }
