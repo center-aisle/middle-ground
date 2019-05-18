ce1448125f30fa4c5004261e5c11fd0c9d3b8df9
NAYWOOD <naywood@Naomis-MacBook-Pro.local>
2019-05-18 14:24:24 -0700

    question carousel, index.html test

--- src/pages/questions.tsx
+++ src/pages/questions.tsx
@@ -4,6 +4,30 @@ function Questions() {
   return (
     <div className='main-container'>
 
+  <div className="carousel carousel-slider center">
+    <div className="carousel-fixed-item center">
+      <a className="btn waves-effect white grey-text darken-text-2">button</a>
+    </div>
+    <div className="carousel-item red white-text" href="#one!">
+      <h2>answerD: "The government should do more to help needy Americans, even if it means going deeper into debt",
+</h2>
+      <p className="white-text">This is your first panel</p>
+    </div>
+    <div className="carousel-item amber white-text" href="#two!">
+      <h2>Second Panel</h2>
+      <p className="white-text">This is your second panel</p>
+    </div>
+    <div className="carousel-item green white-text" href="#three!">
+      <h2>Third Panel</h2>
+      <p className="white-text">This is your third panel</p>
+    </div>
+    <div className="carousel-item blue white-text" href="#four!">
+      <h2>Fourth Panel</h2>
+      <p className="white-text">This is your fourth panel</p>
+    </div>
+  </div>
+        
+
     </div>
   );
 }
