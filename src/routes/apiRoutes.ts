4d28c16fb0224b8e26070fa1c4deb12acbc52e2d
CrypticWoodWhite <13375281+CrypticWoodWhite@users.noreply.github.com>
2019-05-18 10:10:52 -0700

    folders

--- src/routes/apiRoutes.ts
+++ src/routes/apiRoutes.ts
@@ -1,48 +1,10 @@
-const controller = require("../controllers"),
-	passport = require("passport"),
-    OpenIDStrategy = require("passport-openid").Strategy;
-
-var User = require("../models");
-
-var routes = require("express").Router();
+var controller = require("../controllers"),
+    User = require("../models"),
+    routes = require("express").Router();
 
 
 // routes.get(function(req, res) {
 // 	res.send("This is a test");
 // });
 
-/****************************************************
- * LOGIN STUFF BELOW
- * DON'T TOUCH IF YOUR NAME IS NOT CATHERINE
- ******************************************************/
-
-passport.use(new OpenIDStrategy({
-
-    returnURL: "https://bipartisan.herokuapp.com/user",
-    realm: "https://bipartisan.herokuapp.com/",
-    profile: true
-
-}, (identifier: any, profile: { givenName: any; familyName: any; emails: { value: any; }[]; }, done: (arg0: any, arg1: any) => void) => {
-
-    User.findOrCreate({
-        openId: identifier,
-        firstName: profile.givenName,
-        lastName: profile.familyName,
-        email: profile.emails[0].value
-    }, (err: any, user: any) => {
-
-        done(err, user);
-        
-    });
-}));
-
-routes.post("/auth/openid", passport.authenticate("openid"));
-
-routes.get("/auth/openid/return",
-	passport.authenticate("openid", {
-		successRedirect: "/user",
-		failureRedirect: "/" 
-	})
-);
-
-module.exports = routes;
+export default routes;
--- src/server.ts
+++ src/server.ts
@@ -2,33 +2,28 @@ require("dotenv").config();
 const express = require("express"),
     path = require("path"),
     mongoose = require("mongoose");
-var routes = require("./routes"); //uncomment out when apiroutes file is complete
 
-//Initalize express
+var routes = require("./routes");
+
 const app = express(),
     PORT = process.env.PORT || 3001;
 
-//Middleware Parses request Body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("build"));
 };
 
-// Add routes, both API and view
-app.use(routes); // uncomment out when there is more on apiroutes.ts
+app.use(routes);
 app.use(express.static("public"));
-// If no API routes are hit, send the React app
-app.use("/", function (req, res) {
+
+app.use("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-
-// Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/users");
-// Start the API server
-app.listen(PORT, function () {
+
+app.listen(PORT, () => {
     console.log("\uD83C\uDF0E  ==> API Server now listening on PORT " + PORT + "!");
 });
