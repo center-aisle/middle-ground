c65c4319fe4be63d064835b3ddf2b8ac191b088f
NAYWOOD <naywood@Naomis-MacBook-Pro.local>
2019-05-19 11:27:12 -0700

    testing the react-materialize

--- src/pages/questions.tsx
+++ src/pages/questions.tsx
@@ -1,35 +1,46 @@
 import React from 'react';
+import Carousel from 'react-materialize'
 
-function Questions() {
+function Questions1() {
   return (
-    <div className='main-container'>
 
-  <div className="carousel carousel-slider center">
-    <div className="carousel-fixed-item center">
-      <a className="btn waves-effect white grey-text darken-text-2">button</a>
+    <Carousel options={{fullWidth: true,indicators: true}} className="white-text center">
+    <div className="red">
+    <h2>
+    First Panel
+    </h2>
+    <p>
+    This is your first panel
+    </p>
     </div>
-    <div className="carousel-item red white-text" href="#one!">
-      <h2>answerD: "The government should do more to help needy Americans, even if it means going deeper into debt",
-</h2>
-      <p className="white-text">This is your first panel</p>
+    <div className="amber">
+    <h2>
+    Second Panel
+    </h2>
+    <p>
+    This is your second panel
+    </p>
     </div>
-    <div className="carousel-item amber white-text" href="#two!">
-      <h2>Second Panel</h2>
-      <p className="white-text">This is your second panel</p>
+    <div className="green">
+    <h2>
+    Third Panel
+    </h2>
+    <p>
+    This is your third panel
+    </p>
     </div>
-    <div className="carousel-item green white-text" href="#three!">
-      <h2>Third Panel</h2>
-      <p className="white-text">This is your third panel</p>
+    <div className="blue">
+    <h2>
+    Fourth Panel
+    </h2>
+    <p>
+    This is your fourth panel
+    </p>
     </div>
-    <div className="carousel-item blue white-text" href="#four!">
-      <h2>Fourth Panel</h2>
-      <p className="white-text">This is your fourth panel</p>
-    </div>
-  </div>
-        
-
-    </div>
+    </Carousel>
+    
   );
 }
 
-export default Questions;
+
+export default Questions1;
