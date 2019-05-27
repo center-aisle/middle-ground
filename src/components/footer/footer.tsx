65554e20bcf4afb9be18c42176f37d11277f71bb
Efreets <44280987+bryanrkelley@users.noreply.github.com>
2019-05-27 15:13:42 -0700

    potential production fix

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,32 +2,6 @@ import React from 'react';
 import './footer.css';
 
 function Footer() {
-<<<<<<< HEAD
-  return (
-    <footer className='page-footer purple lighten-2'>
-    <div className='container'>
-      <div className='row'>
-        <div className='col l6 s12'>
-          <h4 className='white-text'><a className='black-text text-lighten-4' href='/'>Frenemy</a></h4>
-          <p className='grey-text text-lighten-4'>  Find your new best frenemy!</p>
-        </div>
-        <div className='col l4 offset-l2 s12'>
-          <h5 className='white-text'>Links</h5>
-          <ul>
-            <li><a className='grey-text text-lighten-3' href='/about'>About Us</a></li>
-            <li><a className='grey-text text-lighten-3' href='/contact'>Contact</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    <div className='footer-copyright'>
-      <div className='container'>
-        <p className='grey-text text-lighten-4 right'>© 2019</p>
-      </div>
-    </div>
-  </footer>
-
-=======
 	return (
 		<footer className='page-footer purple lighten-2'>
 			<div className='container'>
@@ -51,7 +25,6 @@ function Footer() {
 				</div>
 			</div>
 		</footer>
->>>>>>> master
 	);
 }
 
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,13 +3,8 @@ import './header.css';
 
 function Header() {
 	return (
-<<<<<<< HEAD
-		<div className='main-container'>
-			<h1><a className='black-text' href='/'>Frenemy</a></h1>
-=======
 		<div className='main-container' id='header'>
 			<h1>Frenemy</h1>
->>>>>>> master
 		</div>
 	);
 }
