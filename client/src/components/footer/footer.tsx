59ee48259b8ab50e043439623cbf8f7171b49c57
NAYWOOD <naywood@naomis-mbp.lan>
2019-05-30 17:20:01 -0700

    added colors and about me

--- client/src/components/footer/footer.tsx
+++ client/src/components/footer/footer.tsx
@@ -3,7 +3,7 @@ import './footer.css';
 
 function Footer() {
 	return (
-		<footer className='page-footer teal darken-1'>
+		<footer className='page-footer green'>
 			<div className='container'>
 				<div className='row'>
 					<div className='col l6 s12'>
